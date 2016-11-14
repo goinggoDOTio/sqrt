0fdb38db1fb17a9a62515cbcd36ba9d9c582af19
Joao Henrique Machado Silva <joaoh82@gmail.com>
2016-11-14 12:38:40 -0200

    finished sqrt program, taking argument

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-func sqrt(x float64) float64 {
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
 
+const DELTA = 1e-15
+
+func sqrt(x float64) float64 {
+	z, d := float64(1), float64(1)
+
+	for d > DELTA {
+		z0 := z
+		z = z - (z*z-x)/(2*z)
+		d = math.Abs(z - z0)
+	}
+	return z
 }
 
 func main() {
-
+	if len(os.Args) > 1 {
+		arg, err := strconv.ParseFloat(os.Args[1], 64)
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+		fmt.Println("The Sqrt of", arg, "is", sqrt(arg))
+	} else {
+		fmt.Println("Please provide a number")
+	}
 }
